d087c28bbc4a2ffaeeaf51322c0f8fa3ee3e6466
Mike <mickelsonmichael@gmail.com>
2016-12-03 08:43:19 -0700

    Initial Commit

--- OSTracker/App_Start/RouteConfig.cs
+++ OSTracker/App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace OSTracker
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
